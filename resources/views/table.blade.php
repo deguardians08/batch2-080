e51d6b72a891c834f8224760bf77da1c332f5da7
Rizky Agestyas Athallah <agestyasrizky@gmail.com>
2018-03-16 16:54:48 +0700

    Initial Commit

---
+++ resources/views/table.blade.php
@@ -0,0 +1,25 @@
+@extends("template")
+@section("content")
+<table class="table table-bordered">
+	<thead>
+		<tr>
+			<th>Kolom Header</th>
+			<th>Kolom Header</th>
+			<th>Kolom Header</th>
+			<th>Kolom Header</th>
+			<th>Kolom Header</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>data</td>
+			<td>data</td>
+			<td>data</td>
+			<td>data</td>
+			<td>data</td>
+		</tr>
+	</tbody>
+
+</table>
+
+@endsection
